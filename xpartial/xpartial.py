42183f69d8bf1b79034a62fe958cc7985e2bba5e
ae <90932525+ea-ae@users.noreply.github.com>
2021-11-09 11:18:56 +0200

    Allow Skips at end of partial

--- xpartial/xpartial.py
+++ xpartial/xpartial.py
@@ -1,7 +1,7 @@
 """Extend functools.partial to allow for selective argument freezing."""
 
 
-__all__ = ['Skip', 'SkipAll', 'xpartial']
+__all__ = ['Skip', 'SkipRest', 'xpartial']
 
 from typing import TypeVar, Callable, Generator, NoReturn, Any
 
@@ -18,7 +18,7 @@ class Skip(Constant):
     """Skip a single positional argument."""
 
 
-class SkipAll(Constant):
+class SkipRest(Constant):
     """Skip as many positional arguments as possible, allowing one to freeze arguments at the end of the function."""
 
 
@@ -34,7 +34,7 @@ def xpartial(func: Callable[..., R], /, *args: Any, **kwargs: Any) -> Callable[.
     :return: Function with frozen arguments.
     """
     def partial_func(*iargs: Any, **ikwargs: Any) -> R:
-        """Partial function with frozen arguments.
+        """Callable partial function with frozen arguments.
 
         :param iargs: Positional arguments.
         :param ikwargs: Keyword arguments.
@@ -43,12 +43,17 @@ def xpartial(func: Callable[..., R], /, *args: Any, **kwargs: Any) -> Callable[.
         def process_args() -> Generator[Any, None, None]:
             """Yield processed function arguments."""
             iarg_it = iter(iargs)
+            remaining = len(args)
             for arg in args:
+                remaining -= 1
                 if arg in ikwargs:
                     yield ikwargs.pop(arg)  # allow overwriting of frozen args if they're present in the kwargs
                 elif arg is Skip:
-                    yield next(iarg_it)
-                elif arg is SkipAll:
+                    try:
+                        yield next(iarg_it)
+                    except StopIteration:
+                        return  # if the remaining args are all Skips then we're fine, if not, TypeError will be raised
+                elif arg is SkipRest:
                     pass
                 else:
                     yield arg
@@ -57,10 +62,12 @@ def xpartial(func: Callable[..., R], /, *args: Any, **kwargs: Any) -> Callable[.
 
         ikwargs |= kwargs
         return func(*process_args(), **ikwargs)
+
+    # print(id(partial_func))
     return partial_func
 
 
 if __name__ == '__main__':
     foo = lambda a, b=0, c=0, d=0, e=0: (a, b, c, d, e)  # noqa: E731
-    f = xpartial(foo, Skip, 5)
-    print(f(2, 333))
+    f = xpartial(foo, 1, Skip, Skip, Skip, SkipRest, Skip)
+    print(f(5))
